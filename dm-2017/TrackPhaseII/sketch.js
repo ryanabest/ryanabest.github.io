4df86f80fc5b212edfeeb6ca3c2560cfd78cacd1
ryanabest <ryanabest@gmail.com>
2017-09-21 13:12:25 -0400

    first add of track phase II

---
+++ dm-2017/TrackPhaseII/sketch.js
@@ -0,0 +1,36 @@
+var width = 375;
+var height = 600;
+
+//Day to Day
+
+var d2d = function( sketch ) {
+
+  sketch.setup = function() {
+    sketch.createCanvas(width, height);
+  };
+
+  sketch.draw = function() {
+    sketch.background(255);
+    sketch.fill(0);
+    sketch.rect(100,100,width/4,height/4);
+  };
+};
+
+var myp5 = new p5(d2d,'p1');
+
+//Overall
+
+var ovr = function( sketch ) {
+
+  sketch.setup = function() {
+    sketch.createCanvas(width, height);
+  };
+
+  sketch.draw = function() {
+    sketch.background(155);
+    sketch.fill(0);
+    sketch.rect(100,100,width/4,height/4);
+  };
+};
+
+var myp5 = new p5(ovr,'p2');
