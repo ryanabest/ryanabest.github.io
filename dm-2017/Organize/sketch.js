b20aca72e2fc77d7e147134e2df654c2e19ba5b0
Ryan Best <ryanbest@Ryans-MacBook-Pro.local>
2017-10-05 19:57:17 -0400

    add csv reference

--- dm-2017/Organize/sketch.js
+++ dm-2017/Organize/sketch.js
@@ -1,9 +1,8 @@
-// function preload() {
-//   // load data from either a local copy of one of the USGS CSVs or directly:
-//   //table = loadTable("assets/discogsdata.csv", "csv", "header");
-//   // or (while you're designing) from the feed itself:
-//   // table = loadTable("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.csv", "csv", "header");
-// }
+function preload() {
+  // load data from either a local copy of one of the USGS CSVs or directly:
+  table = loadTable("assets/discogsdata.csv", "csv", "header");
+  // or (while you're designing) from the feed itself:
+}
 
 var x;
 var y;
