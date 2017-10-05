6952549a7e0b32295d829216dc23de197793f2d5
ryanabest <ryanabest@gmail.com>
2017-10-05 17:25:00 -0400

    add index for organize with p5 test sketch

---
+++ dm-2017/Organize/sketch.js
@@ -0,0 +1,30 @@
+function preload() {
+  // load data from either a local copy of one of the USGS CSVs or directly:
+  discogsdata = loadTable("assets/discogsdata.csv", "csv", "header");
+  // or (while you're designing) from the feed itself:
+  // table = loadTable("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.csv", "csv", "header");
+}
+
+function setup() {
+  rowcount = discogsdata.getRowCount();
+  createCanvas(800,800);
+};
+
+var x = 400;
+var y = 400;
+var h = 100;
+
+function draw() {
+  background(0);
+  fill(255,0,0);
+  rect(x,y,h);
+};
+
+function mousePressed() {
+  for (i=0;i<800;i+=100) {
+    background(0);
+    fill(255,0,0);
+    createCanvas(800,800);
+    rect(x+i,y+i,h);
+  }
+};
