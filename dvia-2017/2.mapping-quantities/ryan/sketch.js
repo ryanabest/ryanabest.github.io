606dfa7cb8f3f28cd6762fb071822de0aa8e5fa9
ryanabest <ryanabest@gmail.com>
2017-10-05 14:54:07 -0400

    test datediff

--- dvia-2017/2.mapping-quantities/ryan/sketch.js
+++ dvia-2017/2.mapping-quantities/ryan/sketch.js
@@ -66,13 +66,10 @@ function draw() {
         Date.daysBetween = function( date1, date2 ) {
           //Get 1 day in milliseconds
           var one_day=1000*60*60*24;
-
-          date1 = Date.parse(date1);
-          date2 = Date.parse(date2);
-
+          
           // Convert both dates to milliseconds
-          var date1_ms = date1.getTime();
-          var date2_ms = date2.getTime();
+          var date1_ms = Date(date1).getTime();
+          var date2_ms = Date(date2).getTime();
 
           // Calculate the difference in milliseconds
           var difference_ms = date2_ms - date1_ms;
