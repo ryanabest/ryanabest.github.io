5b44e9fa88dfd8df03a432c8e220db72ff47bd41
Ryan Best <ryanbest@Ryans-MBP.verizon.net>
2017-10-08 15:28:40 -0400

    add Organization project - working adobe interactive pdf's

--- dm-2017/Organize/sketch.js
+++ dm-2017/Organize/sketch.js
@@ -1,46 +1,98 @@
 function preload() {
   // load data from either a local copy of one of the USGS CSVs or directly:
-  // discogsdata = loadTable("assets/discogsdata.csv", "csv", "header");
-  // tracklist = loadTable("assets/tracklist.csv", "csv", "header");
-  // genres_and_styles = loadTable("assets/genres_and_styles.csv", "csv", "header");
+  discogsdata = loadTable("assets/discogsdata.csv", "csv", "header");
+  tracklist = loadTable("assets/tracklist.csv", "csv", "header");
+  genres_and_styles = loadTable("assets/genres_and_styles.csv", "csv", "header");
   // or (while you're designing) from the feed itself:
-}
-var slider;
-function setup() {
-  // rowcount = discogsdata.getRowCount();
-  // slider = createSlider(0,rowcount,0);
-  imgs = []
-  // for (r=0;r<rowcount;r++) {
-  //   imgs.push("img"+r);
-  // };
-  // for (i=0;i<imgs.length;i++) {
-  //   imgs[i] = loadImage(discogsdata.get(i,"image"));
-  // };
-  img0 = loadImage("https://img.discogs.com/omVDcifhRmbT3rNnsPPD1fXIqig=/fit-in/600x600/filters:strip_icc():format(jpeg):mode_rgb():quality(90)/discogs-images/R-10105639-1491701050-6735.jpeg.jpg");
 };
+
+var colorbutton;
+var artistbutton;
+var artistspecificbutton;
+var imgs = [];
+var val;
+var whoKnows;
+
+//variables to be used in artist function
+var artistlist= [];
+var artistlistsorted = [];
+
+function colorlist() {
+  for (r=0;r<discogsdata.getRowCount();r++) {
+      imgs.push(discogsdata.get(r,"image"));
+  };
+  val = getRandomInt(0,imgs.length);
+  slider = createSlider(0,imgs.length,val);
+  val = slider.value();
+  for (i=0;i<imgs.length;i++) {
+    var img = createImg(imgs[i]);
+    if (i != val) {
+      img.hide();
+    }
+  };
+};
+
+function artists() {
+  //pull all artists into a list
+  for (i=0;i<discogsdata.getRowCount();i++) {
+    artistlist.push(discogsdata.getString(i,"Artist"))
+  };
+
+
+  //sort and remove duplicates
+  artistlist = artistlist.sort();
+  for (a=0;a<artistlist.length;a++) {
+    if (typeof artistlist[a-1] != 'undefined' && artistlist[a-1] != artistlist[a]) {
+      artistlistsorted.push(artistlist[a]);
+    }
+  };
+
+  //create buttons for each artist
+  for (a=0;a<artistlistsorted.length;a++) {
+    artistspecificbutton = createButton(artistlistsorted[a]);
+    artistspecificbutton.mousePressed(artist);
+  }
+};
+
+//
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //The maximum is exclusive and the minimum is inclusive
+}
+
+//generate list of artists for the scroll
+function artist() {
+  imgs = [];
+  for (r=0;r<discogsdata.getRowCount();r++) {
+    if (this.html().replace('&amp;','&') === discogsdata.getString(r,"Artist")) {
+      imgs.push(discogsdata.get(r,"image"));
+    }
+  };
+};
+
+function setup() {
+  artistbutton = createButton("artists");
+  artistbutton.mousePressed(artists);
+  colorbutton = createButton("color");
+  colorbutton.mousePressed(colorlist);
+};
+
+// function keyPressed() {
+//   if (keyCode === RIGHT_ARROW) {
+//     if (val === imgs.length) {
+//       val = 0;
+//     } else {
+//       imgnumber = val + 1;
+//     }
+//   } else if (keyCode === LEFT_ARROW) {
+//     if (val===0) {
+//       val = imgs.length;
+//     } else {
+//       val = val - 1;
+//     }
+//   }
+// };
 
 function draw() {
-  // background(200);
-  // fill(100);
-  // stroke(50);
-  // rect(x,y,w,h);
-  // fill(250,0,0);
-  // rect(x+width,y,w,h);
-  // console.log(x);
-  //x = x - 1;
-  image(img0,0,0);
-  // var val = slider.value();
-  // console.log(val);
 };
-
-// function mousePressed() {
-//   x=x-800;
-//   img = createImg(discogsdata.get(1,"image"));
-//   console.log("Pressed");
-//   // for (i=0;i<800;i+=100) {
-//   //   background(0);
-//   //   fill(255,0,0);
-//   //   createCanvas(800,800);
-//   //   rect(x+i,y+i,w,h);
-//   // }
-// };
