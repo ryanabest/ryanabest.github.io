a4997e3377c2e29d1ea5692795fe6b0b9fb5de6b
Ryan Best <ryanbest@Ryans-MacBook-Pro.local>
2017-11-12 15:21:33 -0500

    add dynamic circle movement

--- dm-2017/Feed/Prototype/sketch.js
+++ dm-2017/Feed/Prototype/sketch.js
@@ -1,41 +1,13 @@
 // list of all stops to map
 var stops;
 
+var colors = [];
 var circles = [];
 
-var currentlat = 40.735310;
-var currentlon = -73.994393;
+var currentlat = 40.76;
+var currentlon = -73.96;
 
 // JQuery to adjust buttons for train direction
-var trainDirection = 'Uptown';
-$(document).ready(function(){
-  $("#uptown").click(function(){
-    $("#uptown").css('background-color','white');
-    $("#uptown").css('color','black');
-    $("#downtown").css('background-color','black');
-    $("#downtown").css('color','white');
-    $(".stopi").css('color','red');
-    $(".stopii").css('color','green');
-    $(".stopiii").css('color','green');
-    $(".stopiv").css('color','white');
-    $(".stopv").css('color','green');
-    trainDirection = 'Uptown';
-    return trainDirection;
-  });
-  $("#downtown").click(function(){
-    $("#downtown").css('background-color','white');
-    $("#downtown").css('color','black');
-    $("#uptown").css('background-color','black');
-    $("#uptown").css('color','white');
-    $(".stopi").css('color','white');
-    $(".stopii").css('color','red');
-    $(".stopiii").css('color','red');
-    $(".stopiv").css('color','white');
-    $(".stopv").css('color','red');
-    trainDirection = 'Downtown';
-    return trainDirection;
-  });
-});
 
 
 function preload() {
@@ -44,9 +16,21 @@ function preload() {
   // console.log(url);
 }
 
+_.mixin({
+    'sortKeysBy': function (obj, comparator) {
+        var keys = _.sortBy(_.keys(obj), function (key) {
+            return comparator ? comparator(obj[key], key) : key;
+        });
+
+        return _.zipObject(keys, _.map(keys, function (key) {
+            return obj[key];
+        }));
+    }
+});
+
 function setup() {
   // create your own map
-  mymap = L.map('subway-map').setView([currentlat, currentlon], 15);
+  mymap = L.map('subway-map', { zoomControl:false }).setView([currentlat, currentlon], 12);
 
   // load a set of map tiles – choose from the different providers demoed here:
   // https://leaflet-extras.github.io/leaflet-providers/preview/
@@ -58,10 +42,14 @@ function setup() {
       // accessToken: 'pk.eyJ1IjoicnlhbmFiZXN0IiwiYSI6ImNqOTdzdWRpcjBhNnMzMmxzcHMyemxkMm0ifQ.ot3NoRC2w8zCbVOCkv2e_w'
   }).addTo(mymap);
 
+  // place current latlon marker
+  // var marker = L.marker([currentlat,currentlon]).addTo(mymap);
+
+  assignAllColors();
+  colors = _.groupBy(colors,'id');
   // draw stops as circles
   for (i=0;i<Object.keys(stops).length;i++) {
     if (stops[Object.keys(stops)[i]]['parent_station'] === '') {
-      var colorNum;
       var lat = stops[Object.keys(stops)[i]]['stop_lat'];
       var lon = stops[Object.keys(stops)[i]]['stop_lon'];
       var name = stops[Object.keys(stops)[i]]['stop_name'];
@@ -70,17 +58,13 @@ function setup() {
       // console.log(Object.keys(stops)[i])
     }
   };
-  // place current latlon marker
-  var marker = L.marker([currentlat,currentlon]).addTo(mymap);
-
-  // noLoop();
-  closestStops();
-
-  // create canvas
+  mapCircles();
+  console.log(circles);
 };
 
 function draw() {
-  // console.log(trainDirection);
+  resizeAllCircles();
+  // console.log(((Math.random() * 0.5) - 0.25));
 }
 
 
@@ -102,82 +86,91 @@ function col(num) {
 function status(num) {
   var stopStatus;
   if (num === 1) {
-    stopStatus = 'Good Service'
+    stopStatus = 'MTA Reports Good Service'
   } else if (num === 2) {
-    stopStatus = 'Minor Delays'
+    stopStatus = 'MTA Reports Minor Delays'
   } else if (num === 3) {
-    stopStatus = 'Major Delays'
+    stopStatus = 'MTA Reports Major Delays'
   } else if (num === 4) {
-    stopStatus = 'Delay Status Unknown'
+    stopStatus = 'MTA Delay Status Unknown'
   };
   return stopStatus;
 }
 
-// distance between two lat lon points per http://www.geodatasource.com/developers/javascript
-function distance(lat1, lon1, lat2, lon2, unit) {
-	var radlat1 = Math.PI * lat1/180
-	var radlat2 = Math.PI * lat2/180
-	var theta = lon1-lon2
-	var radtheta = Math.PI * theta/180
-	var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
-	dist = Math.acos(dist)
-	dist = dist * 180/Math.PI
-	dist = dist * 60 * 1.1515
-	if (unit=="K") { dist = dist * 1.609344 }
-	if (unit=="N") { dist = dist * 0.8684 }
-	return dist
+function assignAllColors() {
+  function assignColor(id) {
+    var colorNum = Math.floor((Math.random() * 4) + 1);
+    var colorPoint  = {
+      id: id,
+      color: col(colorNum),
+      status: status(colorNum)
+    };
+    colors.push(colorPoint);
+  }
+  for (i=0;i<Object.keys(stops).length;i++) {
+    if (stops[Object.keys(stops)[i]]['parent_station'] === '') {
+      assignColor(Object.keys(stops)[i]);
+    }
+  }
 }
+
 
 // draw each stop and add to circles list
 function drawStop (lat,lon,name,id) {
   colorNum = Math.floor((Math.random() * 4) + 1);
   var distUnit = 'K';
-  var currentDist = distance(lat,lon,currentlat,currentlon,distUnit);
+  // var currentDist = distance(lat,lon,currentlat,currentlon,distUnit);
   // console.log(currentDist);
-  var circle = L.circle([lat,lon], {
-    color: col(colorNum),
+  var circle = L.circleMarker([lat,lon], {
+    color: colors[id][0]['color'],
     // fillColor: '#f03',
     fillOpacity: 1,
     stopId: id,
-    stopDist: currentDist,
-    stopName: name
+    // stopDist: currentDist,
+    stopName: name,
+    radius: Number(Math.random() * 4)
   });
-  var popUpText = '<font color = "white" size ="6"><b>' + name + '</b></font><br><font size = "4" color="' + col(colorNum) + '">' + status(colorNum) + '</font><br><br><img src = "assets/TwitterDelayExample.png"/>'
-  circle.addTo(mymap);
+  var popUpText = '<font color = "white" size ="6"><b>' + name + '</b></font><br><font size = "4" color="' + colors[id][0]['color'] + '">' + colors[id][0]['status'] + '</font>'
+  // circle.addTo(mymap);
   circle.bindPopup(popUpText);
   circles.push(circle);
 };
 
-function closestStops(hbColorNum) {
-  stopList = [];
-  for (i=0;i<Object.keys(stops).length;i++) {
-    if (stops[Object.keys(stops)[i]]['parent_station'] === '') {
-      var hbColorNum = Math.floor((Math.random() * 4) + 1);
-      var stopList_lat = stops[Object.keys(stops)[i]]['stop_lat'];
-      var stopList_lon = stops[Object.keys(stops)[i]]['stop_lon'];
-      var stopList_distUnit = 'K';
-      var stopList_data = {
-        stopList_id: Object.keys(stops)[i],
-        stopList_name: stops[Object.keys(stops)[i]]['stop_name'],
-        stopList_dist: distance(stopList_lat,stopList_lon,currentlat,currentlon,stopList_distUnit),
-        stopList_color: col(hbColorNum),
-        stopList_status: status(hbColorNum)
-      }
-      stopList.push(stopList_data);
-    };
-  };
-  stopList = _.orderBy(stopList,['stopList_dist'],['asc']);
-  var hbStopList = [];
-  for (j=0;j<5;j++){
-    hbStopList_data = {
-      hbStopList_id: stopList[j]['stopList_id'],
-      hbStopList_name: stopList[j]['stopList_name'],
-      hbStopList_dist: stopList[j]['stopList_dist'],
-      hbStopList_color: stopList[j]['stopList_color'],
-      hbStopList_status: stopList[j]['stopList_status']
-    };
-    hbStopList.push(hbStopList_data);
-  };
-  console.log(hbStopList);
-  return(hbStopList);
+function mapCircles() {
+  // circles = _.sortBy(circles,'_radius','desc');
+  circles.forEach(function(circle, i){
+    circle.addTo(mymap);
+  });
+}
+
+
+function removeAllCircles(){
+    // remove each circle from the map and empty our array of references
+    circles.forEach(function(circle, i){
+        mymap.removeLayer(circle);
+    })
+    circles = [];
 };
+
+function resizeAllCircles() {
+  circles.forEach(function(circle, i) {
+    var resizeNum = resize(circle['_radius']);
+    circle.setRadius(resizeNum);
+    circle['_radius'] = resizeNum;
+  })
+}
+
+function resize(num) {
+  num = num + ((Math.random() * 0.4) - 0.2);
+  if (num < 0) {
+    num = 0
+  }
+  if (num > 10) {
+    num = 0
+  }
+  return num;
+}
+
+// function reorder() {
+//   circles = _.
+// }
