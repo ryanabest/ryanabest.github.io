f8348a47b757b377b5556e000a58b79e284bd8c7
ryanabest <ryanabest@gmail.com>
2018-05-12 14:07:22 -0400

    progress on site design and work. migration of art still needs visual design section and additional photos

---
+++ js/link-hover.js
@@ -0,0 +1,23 @@
+$("#twitter-thumb").mouseover(function() {
+  $(this).attr('src',"../assets/twitter-red.png")
+}).mouseout(function() {
+  $(this).attr('src',"../assets/twitter.png")
+})
+
+$("#linkedin-thumb").mouseover(function() {
+  $(this).attr('src',"../assets/linkedin-red.png")
+}).mouseout(function() {
+  $(this).attr('src',"../assets/linkedin.png")
+})
+
+$("#tableau-thumb").mouseover(function() {
+  $(this).attr('src',"../assets/tableau-red.png")
+}).mouseout(function() {
+  $(this).attr('src',"../assets/tableau.png")
+})
+
+$("#github-thumb").mouseover(function() {
+  $(this).attr('src',"../assets/github-red.png")
+}).mouseout(function() {
+  $(this).attr('src',"../assets/github.png")
+})
