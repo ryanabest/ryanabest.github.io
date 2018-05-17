4bb72ceafa02eedc920cf60833d56088d75b1e54
ryanabest <ryanabest@gmail.com>
2018-05-17 00:18:02 -0400

    edit text for JSON data animation

--- js/migration-data.js
+++ js/migration-data.js
@@ -7,7 +7,7 @@ function showDataProcess() {
   $('#prov-text-ex-img').children('img').animate({opacity: 1}, 6000, function() {
     $("#data-process-text").children("p").text("I used python to extract the time and place of each location change for both ownership changes and exhibitions");
       $('#python-ex-img').children('img').animate({opacity: 1}, 6000, function() {
-        $("#data-process-text").children("p").text("Then combined ownership and exhibition locations to generate clean JSON files that would feed into my front-end");
+        $("#data-process-text").children("p").text("Then combined ownership and exhibitions to generate clean JSON files that would feed into my front-end visualization");
           $('#json-ex-img').children('img').animate({opacity: 1}, 6000, function() {
               setTimeout(function(){ showDataProcess(); }, 6000);
             });
