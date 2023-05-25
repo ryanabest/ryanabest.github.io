0f42b7dbab636227db8d9a6d85436268286a03a1
ryan <ryanabest@gmail.com>
2023-05-25 10:54:07 -0400

    add fte icon

--- js/link-hover.js
+++ js/link-hover.js
@@ -1,36 +1,36 @@
-let twitter  = document.getElementById("twitter-thumb")
-let linkedIn = document.getElementById("linkedin-thumb")
-let tableau  = document.getElementById("tableau-thumb")
-let gitHub   = document.getElementById("github-thumb")
+const twitter = document.getElementById('twitter-thumb');
+const linkedIn = document.getElementById('linkedin-thumb');
+const gitHub = document.getElementById('github-thumb');
+const fte = document.getElementById('fte-thumb');
 
-twitter.addEventListener("mouseover",function(event) {
-  twitter.src = "/assets/social-icons/twitter-highlight.png"
-})
+twitter.addEventListener('mouseover', function (event) {
+  twitter.src = '/assets/social-icons/twitter-highlight.png';
+});
 
-twitter.addEventListener("mouseout",function(event) {
-  twitter.src = "/assets/social-icons/twitter.png"
-})
+twitter.addEventListener('mouseout', function (event) {
+  twitter.src = '/assets/social-icons/twitter.png';
+});
 
-linkedIn.addEventListener("mouseover",function(event) {
-  linkedIn.src = "/assets/social-icons/linkedin-highlight.png"
-})
+linkedIn.addEventListener('mouseover', function (event) {
+  linkedIn.src = '/assets/social-icons/linkedin-highlight.png';
+});
 
-linkedIn.addEventListener("mouseout",function(event) {
-  linkedIn.src = "/assets/social-icons/linkedin.png"
-})
+linkedIn.addEventListener('mouseout', function (event) {
+  linkedIn.src = '/assets/social-icons/linkedin.png';
+});
 
-tableau.addEventListener("mouseover",function(event) {
-  tableau.src = "/assets/social-icons/tableau-highlight.png"
-})
+gitHub.addEventListener('mouseover', function (event) {
+  gitHub.src = '/assets/social-icons/github-highlight.png';
+});
 
-tableau.addEventListener("mouseout",function(event) {
-  tableau.src = "/assets/social-icons/tableau.png"
-})
+gitHub.addEventListener('mouseout', function (event) {
+  gitHub.src = '/assets/social-icons/github.png';
+});
 
-gitHub.addEventListener("mouseover",function(event) {
-  gitHub.src = "/assets/social-icons/github-highlight.png"
-})
+fte.addEventListener('mouseover', function (event) {
+  fte.src = '/assets/social-icons/fivethirtyeight-highlight.png';
+});
 
-gitHub.addEventListener("mouseout",function(event) {
-  gitHub.src = "/assets/social-icons/github.png"
-})
+fte.addEventListener('mouseout', function (event) {
+  fte.src = '/assets/social-icons/fivethirtyeight.png';
+});
