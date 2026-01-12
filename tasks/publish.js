7457b0233604cbb842a1cc46405ec16170dbe463
ryanabest <ryanabest@gmail.com>
2026-01-12 14:47:26 -0500

    downgrade gh-pages version

--- tasks/publish.js
+++ tasks/publish.js
@@ -1,5 +1,14 @@
 const ghpages = require('gh-pages');
 
-ghpages.publish('dist', (err) => {
-  console.log(err);
+ghpages.publish('dist', {
+  // Disable deployment status check to avoid stalling
+  nojekyll: true,
+  dotfiles: true
+}, (err) => {
+  if (err) {
+    console.error('Deployment failed:', err);
+    process.exit(1);
+  } else {
+    console.log('Deployment successful!');
+  }
 });
