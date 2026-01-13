d76d7c2b8466fb538b44aca86388087de3f2cbd4
ryanabest <ryanabest@gmail.com>
2026-01-13 14:18:37 -0500

    update publish

--- tasks/publish.js
+++ tasks/publish.js
@@ -1,14 +1,18 @@
 const ghpages = require('gh-pages');
 
 ghpages.publish('dist', {
-  // Disable deployment status check to avoid stalling
   nojekyll: true,
-  dotfiles: true
+  dotfiles: true,
+  // Skip deployment status checks to prevent hanging
+  silent: false,
+  remove: '.',
+  history: false
 }, (err) => {
   if (err) {
     console.error('Deployment failed:', err);
     process.exit(1);
   } else {
-    console.log('Deployment successful!');
+    console.log('Files pushed to gh-pages branch successfully!');
+    console.log('GitHub Pages will deploy shortly.');
   }
 });
